234f6925df1e119ca9e8ee1612f16f6dd6b3f1e6
Alexandre BonzomBotify <alexandre.bonzom@botify.com>
2022-09-13 16:56:03 +0200

    correcting files

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,18 @@
-FROM jarredsumner/bun:edge
+
+FROM frolvlad/alpine-glibc:glibc-2.30
+
+WORKDIR /bun
+RUN apk --no-cache add curl bash libstdc++ ca-certificates && \
+    curl -fsSL -o "/bun/bun.zip" "https://github.com/Jarred-Sumner/bun/releases/download/bun-v0.1.11/bun-linux-x64.zip" && \
+    unzip -d /bun -q -o "/bun/bun.zip" && \
+    mv /bun/bun-linux-x64/bun /usr/local/bin/bun && \
+    chmod 777 /usr/local/bin/bun && \
+    rm "/bun/bun.zip" && \
+    apk del curl bash
+
 WORKDIR /app
+
 COPY package.json package.json
-RUN bun install
+
 COPY . .
-EXPOSE 3000
-ENTRYPOINT ["bun", "index.js"]
+CMD ["bun","run","index.ts"]
--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
-import moment from 'moment';
-
 export default {
-  port: 3000,
+  port: Number(process.env.PORT ?? 3000),
   fetch(req) {
     const url = new URL(req.url);
-    const now = moment().format('YYYY-MM-DD HH:mm:ss');
+    const now = new Date().toISOString();
     return new Response(
       `
       <html>
@@ -12,6 +10,7 @@ export default {
           <img src="https://bun.sh/logo@2x.png" style="height: 48px;" alt="bun logo" />
           <h1>Basic example of Bun</h1>
           <div>Rendered at: ${now}</div>
+          <div>This is ${url.toString()}.</div>
         </main>
       </html>`,
       {
@@ -21,5 +20,4 @@ export default {
       }
     );
   },
-  }
 };
